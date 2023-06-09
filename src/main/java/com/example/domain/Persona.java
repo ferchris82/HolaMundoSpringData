c16693cfd3cc5e4fdcf4c38a459a59e2951a97aa
Christian <ferchris82@hotmail.com>
2023-06-08 21:21:28 -0500

    Connection MySQL with Spring

---
+++ src/main/java/com/example/domain/Persona.java
@@ -0,0 +1,28 @@
+package com.example.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.io.Serializable;
+import lombok.Data;
+
+
+
+@Data
+@Entity
+@Table(name = "persona")
+public class Persona implements Serializable{
+    
+    private static final long serialVersionUID = 1L;
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id_persona;
+    private String nombre;
+    private String apellido;
+    private String email;
+    private String telefono;
+    
+}
